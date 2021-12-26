093b7ca01eca774264102dc12903880b27332ea9
Sreenaina Koujala <nkoujala@gmail.com>
2021-12-26 18:29:08 -0500

    update

--- lesson1-history.js
+++ lesson1-history.js
@@ -1,1 +1,44 @@
-// write about history
+/*
+
+What is JavaScript?
+
+JavaScript is very important and for the 7th year in a row, has been ranked the most commonly
+used programming language. It has been used to build almost 95% of websites and without
+it the several helpful web applications would not have the power they have today. JS is a
+scripting language that helps add functionality and behaviors to the website, allowing viewers 
+to interact with web content in a unique way. It is also a client-side language, meaning it 
+runs on a computer within the browseer mainly. JS is also very easy to learn, hence why it
+surpassed many previous languages. 
+
+Origins of JavaScript
+
+The internet was booming in the mid-1990s, as Microsoft and Netscape were engaged in browser wars.
+In September 1995, Netscape programmer Brandan Eich developed a new language in just 10 days (crazy), 
+and originally named it Mocha (oo coffee), then known as LiveScript, and now finally JavaScript. 
+However, JavaScript should not be confused with Java as the two have barely anytning in common.
+JavaScript is named after Netscape's support of Java applets within the browser. JavaScript is also
+much more dynamic as it was created to be interpreted at run time which Java code must first be compiled.
+JS also wasn't perfect at first as many saw it as "UI Glue" and not a true language. But it helped the
+internet flourish and therefore it spread rapidly soon after. In 1997, JavaScript grew so rapidly that
+the need for proper management and maintenance was needed. Netscape created ECMA(several versions) 
+which stands for European Computer Manufacturers Association, whose goal was standardized computing. 
+Ajax was introduced in 2005, which was supposed to improve user experience by allowing web pages to feel 
+more native like desktopapps. This helped push JavaScript in becoming a professional programming language. 
+JS had challenges too withincompatibility issues on browsers, so the CS community responded with frameworks 
+and libraries to help solve those issues. JavaScript may have had a slightly rocky start, but it became one 
+of the best programming languages and will be for years to come. 
+
+Why is JavaScript important?
+
+JavaScript is one of the crucial languages web developers must learn alongside HTML and CSS
+HTML describes the content of web pages, accesses information through links, and uses tags
+to tell the browser how to display the information. CSS specifies the layout of web pages 
+and how the information should be displayed with a specific formatting style. JavaScript is
+helpful in programming the behavior of web pages, using HTML and CSS to help it perform its 
+useful methods. 
+
+sources: 
+https://www.springboard.com/blog/data-science/history-of-javascript/
+https://www.w3schools.com/js/default.asp
+
+*/
